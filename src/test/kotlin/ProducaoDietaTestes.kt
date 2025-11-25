b53147c871f0cf69bab54a730134ca9c67cb7948
Andrew Ramires May <andrewc3po@gmail.com>
2025-11-25 17:03:19 -0400

    Refatoração no teste

--- src/test/kotlin/ProducaoDietaTestes.kt
+++ src/test/kotlin/ProducaoDietaTestes.kt
@@ -1,18 +1,18 @@
+import agromobi.AgromobiSession
+import agromobi.core.ButtonByTextValue
 import com.codeborne.selenide.Condition.exactText
 import com.codeborne.selenide.Condition.visible
 import com.codeborne.selenide.Configuration
-import com.codeborne.selenide.ElementsCollection
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import com.codeborne.selenide.Selenide.*
-import com.codeborne.selenide.Selectors.byPartialLinkText
 import config.DogConfiguration
 import org.openqa.selenium.chrome.ChromeOptions
 import repositories.DietaRepository
 import kotlin.test.AfterTest
 import kotlin.test.assertEquals
 import com.codeborne.selenide.Selenide.`$$`
-import com.codeborne.selenide.SelenideElement
+import util.waitForResult
 
 class TesteInicial {
     companion object {
@@ -25,7 +25,7 @@ class TesteInicial {
         @JvmStatic
         fun setup() {
             Configuration.browser = "chrome"
-            Configuration.headless = false
+            Configuration.headless = true
             Configuration.holdBrowserOpen = true
             Configuration.browserSize = "1280x800"
             Configuration.fastSetValue = true
@@ -46,73 +46,25 @@ class TesteInicial {
 
     @Test
     fun `abre navegador e interage visivelmente`() {
-        // Abre a página
-        open("https://gruporovema.hom.katrid.com.br/web/login/?next=/web/")
 
-        `$`("#id-username").shouldBe(visible).setValue(dogConfiguration.username)
-        `$`("#id-password").shouldBe(visible).setValue(dogConfiguration.password)
-        `$`(".btn").click()
-        `$`(".fa-duotone").shouldBe(visible)
+        var dietaIdInt : Int = -1
+        var totalUi : Double = -1.0
+
+        AgromobiSession(dogConfiguration.username,dogConfiguration.password)
+            .login()
+            .producaoDieta()
+            .criaDietaValida()
+            .refresh()
+            .encerrar()
+            .refresh().also {
+                dietaIdInt = it.getId()
+                totalUi = it.getTotal()
+            }
 
 
-        // redireciona para o menu desejado após login bem-sucedido
-        open(producaoDieta)
-        `$`(".btn-action-create").shouldBe(visible).click()
-        //TODO data nao foi selecionada
-        `$`("#k-input-4").shouldBe(visible).selectOptionContainingText("Manual")
-
-
-        TesteEmcapsula("#k-input-6","f008","QTH2B58")
-
-        TesteEmcapsula("#k-input-7","SERRA VERDE")
-        TesteEmcapsula("#k-input-8","ITAMAR DOMICIANO")
-        TesteEmcapsula("#k-input-9","TERMINAÇÃO 3 - B")
-
-        `$`(".btn-action-add").click()
-        TesteEmcapsula("#k-input-21","MILHO")
-
-        `$`("#k-input-22").setValue("100")
-        `$`("#k-input-23").setValue("100")
-
-        `$`("body > div.modal.form-view.editable.show.changing.inserting > div > div > div.modal-footer > button:nth-child(1)").click()
-
-        `$`(".btn-action-save").shouldBe(visible).click()
-
-        `$`(".btn-action-refresh").shouldBe(visible).click()
-
-        //Fazer um wrapper para esse cara, click by text value!
-        `$$`("button").findBy(exactText("Encerrar")).shouldBe(visible).click()
-
-        Thread.sleep(1000) // 1s pause after click
-
-        `$`(".btn-action-refresh").shouldBe(visible).click()
-
-        val dietaID = `$`("section.form-field-section:nth-child(2) > div:nth-child(3) > span:nth-child(1)").shouldBe(visible).text()
-        println("ID da dieta na tela = $dietaID")
-
-        val TotalDieta = `$`("section.form-field-section:nth-child(15) > div:nth-child(3)").shouldBe(visible).text()
-        println("Total da dieta na tela = $TotalDieta")
-        val totalSegundos = 2 * 60
-        for (restante in totalSegundos downTo 1) {
-            println("Aguardando verificação no banco... faltam ${restante}s")
-            Thread.sleep(1000)
-        }
-
-        val dietaIdInt = dietaID.trim().toInt()
-
-// total da tela -> normalizar para Double
-        val totalUi = TotalDieta
-            .replace("R$", "")  // se tiver símbolo de moeda
-            .replace(" ", "")
-            .replace(".", "")   // tira separador de milhar
-            .replace(",", ".")  // vírgula decimal -> ponto
-            .toDouble()
-
-        println("Total da dieta na tela (double) = $totalUi")
-
-// total no banco
-        val totalDb = DietaRepository.buscarTotalPorDieta(dietaIdInt)
-            ?: error("Nenhum total encontrado no banco para dieta ID=$dietaIdInt")
+        val totalDb = waitForResult {
+            DietaRepository.buscarTotalPorDieta(dietaIdInt)
+        } ?: error("Nenhum total encontrado no banco para dieta ID=$dietaIdInt")
 
         println("Total da dieta no banco = $totalDb")
 
@@ -122,6 +74,7 @@ class TesteInicial {
             0.01,
             "Total da dieta diferente entre UI ($totalUi) e banco ($totalDb) para ID=$dietaIdInt"
         )
+
         val itensDb = DietaRepository.buscarItensPorDieta(dietaIdInt)
 
 // agrupa por descrição
@@ -167,21 +120,6 @@ class TesteInicial {
         }
         closeWindow()
 
-
-//        #action-manager > action-view > div > div.content > header > button:nth-child(1)
-
-//        #action-manager > action-view > div > div.data-heading.panel.panel-default > div.panel-body > div > div:nth-child(1) > div > div > button.btn.btn-primary
-//        `$`(".fa-duotone").click()
-//        `$`("#ui-menu-456 > img").click()
-//        `$`("#ui-menu-529").click()
-//        `$`(".toolbar").click()
-//
-//        `$`("#ui-menu-529").click()
-
-//        id=ui-menu-529
-
-
-        // dá um tempinho pra você ver o que aconteceu
         Thread.sleep(50000)
     }
 
@@ -192,18 +130,8 @@ class TesteInicial {
 }
 
 
-class TesteEmcapsula(val inputSelector : String, val valueType : String, val textLinkClick : String? = null){
 
-    init{
-        val field = `$`(inputSelector).shouldBe(visible)
-        field.click()
-        field.type(valueType)
-        `$`(byPartialLinkText(textLinkClick ?: valueType))
-            .shouldBe(visible)
-            .click()
-    }
 
-}
 data class ItemUi(
     val description: String,
     val quantity: Double,
